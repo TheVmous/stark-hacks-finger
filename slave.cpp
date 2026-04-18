a82fbc034831bd3f21ea0238279a445f7222e25f
Vmous <vedjoshi2007@gmail.com>
2026-04-18 18:15:52 -0400

    fix: partially migrated from BluetoothSerial to NimBLE and implemented master-seeking

--- slave.cpp
+++ slave.cpp
@@ -1,13 +1,32 @@
 #include <Arduino.h>
-#include <BluetoothSerial.h>
+#include <NimBLEDevice.h>
 #include <ESP32Servo.h>
 
-BluetoothSerial SerialBT;
 Servo myservo;
 
 void setup() {
   Serial.begin(115200);
-  SerialBT.begin("Slave");
+  NimBLEDevice::init("");
+
+  NimBLEScan *pScan = NimBLEDevice::getScan();
+  NimBLEScanResults results = pScan->getResults(10 * 1000); //10 sec scan for devices
+  NimBLEUUID masterId("master");
+
+  //iterating through results untill we find the desired UUID
+  for (int i = 0; i < results.getCount(); i++) {
+    const NimBLEAdvertisedDevice *device = results.getDevice(i);
+
+    if (device->isAdvertisingService(masterId)) {
+      NimBLEClient *pClient = NimBLEDevice::createClient();
+
+      if (!(pClient->connect(&device))) {
+        Serial.println("Failed to connect, restarting...");
+        ESP.restart();
+      } else {
+        Serial.println("Connected!");
+      }
+    }
+  }
 
   myservo.setPeriodHertz(50);
   myservo.attach(13, 500, 3000);
