771310f1e809a6af6ffaecfcf81af75cd1a46104
Ved J <141575804+TheVmous@users.noreply.github.com>
2026-04-18 22:10:53 -0400

    fix: fully migrated BLE functionality with callbacks added
    Mostly repurposed official BLE documentation code, to be tested.

--- cremy.cpp
+++ cremy.cpp
@@ -1,68 +1,174 @@
+//This is heavily heavily adapted from https://github.com/espressif/arduino-esp32/blob/master/libraries/BLE/examples/Client/Client.ino
+
 #include <Arduino.h>
-#include <NimBLEDevice.h>
+#include "BLEDevice.h"
 #include <ESP32Servo.h>
+#define CLOSED 180
+#define OPEN 0
+
+// The remote service we wish to connect to.
+static BLEUUID serviceUUID("oh-great-master");
+
+// The characteristic of the remote service we are interested in.
+static BLEUUID charUUID("switch-status");
+
+static boolean doConnect = false;
+static boolean connected = false;
+static boolean doScan = false;
+static BLERemoteCharacteristic *pRemoteCharacteristic;
+static BLEAdvertisedDevice *myDevice;
 
 Servo myservo;
-NimBLECLient *pClient = nullptr;
+//NimBLECLient *pClient = nullptr;
+
+// Callback function to handle notifications
+static void notifyCallback(BLERemoteCharacteristic *pBLERemoteCharacteristic, uint8_t *pData, size_t length, bool isNotify) {
+  Serial.print("Notify callback for characteristic ");
+  Serial.print(pBLERemoteCharacteristic->getUUID().toString().c_str());
+  Serial.print(" of data length ");
+  Serial.println(length);
+  Serial.print("data: ");
+  Serial.write(pData, length);
+  Serial.println();
+}
+
+class MyClientCallback : public BLEClientCallbacks {
+  void onConnect(BLEClient *pclient) {}
+
+  void onDisconnect(BLEClient *pclient) {
+    connected = false;
+    Serial.println("onDisconnect");
+  }
+};
+
+bool connectToServer() {
+  Serial.print("Forming a connection to ");
+  Serial.println(myDevice->getAddress().toString().c_str());
+
+  BLEClient *pClient = BLEDevice::createClient();
+  Serial.println(" - Created client");
+
+  pClient->setClientCallbacks(new MyClientCallback());
+
+  // Connect to the remove BLE Server.
+  pClient->connect(myDevice);  // if you pass BLEAdvertisedDevice instead of address, it will be recognized type of peer device address (public or private)
+  Serial.println(" - Connected to server");
+  pClient->setMTU(517);  //set client to request maximum MTU from server (default is 23 otherwise)
+
+  // Obtain a reference to the service we are after in the remote BLE server.
+  BLERemoteService *pRemoteService = pClient->getService(serviceUUID);
+  if (pRemoteService == nullptr) {
+    Serial.print("Failed to find our service UUID: ");
+    Serial.println(serviceUUID.toString().c_str());
+    pClient->disconnect();
+    return false;
+  }
+  Serial.println(" - Found our service");
+
+  // Obtain a reference to the characteristic in the service of the remote BLE server.
+  pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
+  if (pRemoteCharacteristic == nullptr) {
+    Serial.print("Failed to find our characteristic UUID: ");
+    Serial.println(charUUID.toString().c_str());
+    pClient->disconnect();
+    return false;
+  }
+  Serial.println(" - Found our characteristic");
+
+  // Read the value of the characteristic.
+  if (pRemoteCharacteristic->canRead()) {
+    String value = pRemoteCharacteristic->readValue();
+    Serial.print("The characteristic value was: ");
+    Serial.println(value.c_str());
+
+    //TODO: can add servo stuff here I believe
+  }
+
+  if (pRemoteCharacteristic->canNotify()) {
+    // Register/Subscribe for notifications
+    pRemoteCharacteristic->registerForNotify(notifyCallback);
+  }
+
+  connected = true;
+  return true;
+}
+
+/**
+ * Scan for BLE servers and find the first one that advertises the service we are looking for.
+ */
+class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
+  /**
+   * Called for each advertising BLE server.
+   */
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+    Serial.print("BLE Advertised Device found: ");
+    Serial.println(advertisedDevice.toString().c_str());
+
+    // We have found a device, let us now see if it contains the service we are looking for.
+    if (advertisedDevice.haveServiceUUID() && advertisedDevice.isAdvertisingService(serviceUUID)) {
+
+      BLEDevice::getScan()->stop();
+      myDevice = new BLEAdvertisedDevice(advertisedDevice);
+      doConnect = true;
+      doScan = true;
+
+    }  // Found our server
+  }  // onResult
+};  // MyAdvertisedDeviceCallbacks
+
+
+
 
 void setup() {
   Serial.begin(115200);
-  NimBLEDevice::init("");
+  Serial.println("Searching for Master...");
+  BLEDevice::init("");
 
-  NimBLEScan *pScan = NimBLEDevice::getScan();
-  NimBLEScanResults results = pScan->getResults(10 * 1000); //10 sec scan for devices
-  NimBLEUUID masterId("master");
-
-  //iterating through results untill we find the desired UUID
-  for (int i = 0; i < results.getCount(); i++) {
-    const NimBLEAdvertisedDevice *device = results.getDevice(i);
-
-    if (device->isAdvertisingService(masterId)) {
-      pClient = NimBLEDevice::createClient();
-
-      if (!(pClient->connect(&device))) {
-        Serial.println("Failed to connect, restarting...");
-        ESP.restart();
-      } else {
-        Serial.println("Connected!");
-      }
-    }
-  }
+  // Retrieve a Scanner and set the callback we want to use to be informed when we
+  // have detected a new device.  Specify that we want active scanning and start the
+  // scan to run for 5 seconds.
+  BLEScan *pBLEScan = BLEDevice::getScan();
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setInterval(1349);
+  pBLEScan->setWindow(449);
+  pBLEScan->setActiveScan(true);
+  pBLEScan->start(5, false);
 
   myservo.setPeriodHertz(50);
   myservo.attach(13, 500, 3000);
   //might need to callibrate these min maxes to our servo
   myservo.write(0); //open position
-
-  while (!SerialBT.connected()) {
-    delay(1000);
-    Serial.println("Slave is waiting for Master to connect...");
-  }
-
-  Serial.println("Connected to Master.");
-}
+}  // End of setup.
 
 void loop() {
-  NimBLECLient localClient = pClient;
-  //not sure if I need this line..
 
-  if (localclient != nullptr && localClient->isConnected()) {
-
-    //code to be migrated
-    if (SerialBT.available()) {
-      char input = SerialBT.read();
-
-      if (input == '1') {
-        myservo.write(180);  //close
-        SerialBT.println("Finger closed");
-      } else if (input == '0') {
-        myservo.write(0);  //open
-        SerialBT.println("Finger open");
-      }
+  // If the flag "doConnect" is true then we have scanned for and found the desired
+  // BLE Server with which we wish to connect.  Now we connect to it.  Once we are
+  // connected we set the connected flag to be true.
+  if (doConnect == true) {
+    if (connectToServer()) {
+      Serial.println("We are now connected to the BLE Server.");
+    } else {
+      Serial.println("We have failed to connect to the server; there is nothing more we will do.");
+      //Originally I would have put something like ESP.restart() but I think with callbacks it'd mess up
     }
-
-    delay(50);
+    doConnect = false;
   }
 
-  
-}
+  // If we are connected to a peer BLE Server, update the characteristic each time we are reached
+  // with the current time since boot.
+  if (connected) {
+    std::string output = pRemoteCharacteristic->readValue();
+    if (val == "1") {
+      myservo.write(CLOSED);
+      Serial.println("finger closed");
+    } else if (vall == "0") {
+      myservo.write(OPEN);
+      Serial.println("finger opened");
+    }
+  } else {
+    ESP.restart(); //if disconnect, restart
+  }
+
+  delay(500);  // Delay half a second between loops.
+}  // End of loop
